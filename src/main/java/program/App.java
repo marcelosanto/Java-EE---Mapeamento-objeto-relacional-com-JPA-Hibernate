0ee8858631a5919324942e13d1a95542e1109973
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-27 14:07:40 -0300

    Java EE - Mapeamento objeto-relacional com JPA / Hibernate

--- src/main/java/program/App.java
+++ src/main/java/program/App.java
@@ -9,19 +9,32 @@ import javax.persistence.Persistence;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Pessoa p1 = new Pessoa(null, "Marcello", "marcelo@gmail.com");
+       /* Pessoa p1 = new Pessoa(null, "Marcello", "marcelo@gmail.com");
         Pessoa p2 = new Pessoa(null, "Gabriel", "gabriel@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Alice", "alice@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Alice", "alice@gmail.com");*/
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
+        /** - Salvando no banco de dados
         em.getTransaction().begin();
         em.persist(p1);
         em.persist(p2);
         em.persist(p3);
         em.getTransaction().commit();
+         **/
+
+        //Pesquisando no banco de dados
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
+        //Apagando no banco de dados
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
 
         System.out.println("Pronto");
+        em.close();
+        emf.close();
     }
 }
